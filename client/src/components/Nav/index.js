76a9774e6f7ab3dfebd288e4bef56036119f4c9a
Julian <cusco2891@gmail.com>
2020-04-09 05:17:48 -0400

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,9 +9,9 @@ function Nav() {
       </div>
       <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Work</a></li>
+        <li><a href="/">Projects</a></li>
       </ul>
     </nav>
   );
