c9c2b518ef2cbd3d402c092234105f07aed339fb
Julian <cusco2891@gmail.com>
2020-04-09 05:16:22 -0400

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 function Nav() {
   return (
     <nav className="nav navbar-expand-lg navbar-dark">
-
+      <div className="logo">
+      <h4>the nav</h4>
+      </div>
     </nav>
   );
 }
