63094e40ca5f12ca51969d25d9ef662b87c50f62
Julian <cusco2891@gmail.com>
2020-04-10 04:00:39 -0400

    nav class

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -13,6 +13,11 @@ function Nav() {
         <li><a href="#">Work</a></li>
         <li><a href="#">Projects</a></li>
       </ul>
+      <div className="burger">
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+      </div>
     </nav>
   );
 }
