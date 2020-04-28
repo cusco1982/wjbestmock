a3233c6293f3090c7cfc05595377537141d6b897
Julian <cusco2891@gmail.com>
2020-04-28 07:23:23 -0400

    menu data

---
+++ client/src/components/Nav/menu-data.js
@@ -0,0 +1,18 @@
+export const menuData = [
+    {
+        label: 'Home',
+        url:'https://www.google.com'
+    },
+    {
+        label: 'Services',
+        url:'https://www.google.com'
+    },
+    {
+        label: 'Projects',
+        url:'https://www.google.com'
+    },
+    {
+        label: 'Contact Us',
+        url:'https://www.google.com'
+    }
+]
