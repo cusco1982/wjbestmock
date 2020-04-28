bfd2f1eb4d018f8dc009eac359e81275b01ed88f
Julian <cusco2891@gmail.com>
2020-04-28 06:54:16 -0400

    check

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,23 +7,8 @@ function Nav() {
 
 
   return (
-    <nav>
-      <div className="logo">
-        <h4>the nav</h4>
-      </div>
-      <div className="phone-number">1-732-777-4444</div>
-      <ul className='nav-links'>
-        <li><a href="/">Home</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact Us</a></li>
-      </ul>
-      <div className="burger">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
+
+    <div></div>
   );
 }
 
