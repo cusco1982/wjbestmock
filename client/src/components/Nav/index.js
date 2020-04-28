dd69ef10ee7c3de606f9c84165619db33e0225a0
Julian <cusco2891@gmail.com>
2020-04-27 23:55:29 -0400

    clear

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,40 +3,6 @@ import "./style.css";
 
 
 
-
-
-
-// const navSlide = () => {
-//   const burger = document.querySelector('.burger');
-//   const nav = document.querySelector('.nav-links');
-//   const navLinks = document.querySelectorAll('.nav-links li');
-
-
-
-//   burger.addEventListener('click', () => {
-
-//     // toggle nav
-//     nav.classList.toggle('nav-active');
-
-//     // animate links
-//     navLinks.forEach((link, index) => {
-//       if (link.style.animation) {
-//         link.style.animation = '';
-//       } else {
-//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-//       }
-//     });
-//     // burger animation
-//     burger.classList.toggle('toggle');
-//   });
-// }
-
-// navSlide();
-
-
-
-
-
 function Nav() {
 
 
