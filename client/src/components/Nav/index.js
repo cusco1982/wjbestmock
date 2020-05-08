e77d4dbe092bfe4da322a83cf431da43320aa28f
Julian <cusco2891@gmail.com>
2020-05-07 21:54:23 -0400

    check

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,34 +1,34 @@
-import React from "react";
-import "./style.css";
-import { menuData } from "./menu-data.js"
+// import React from "react";
+// import "./style.css";
+// import { menuData } from "./menu-data.js"
 
 
 
-class Nav extends React.Component {
-  render() {
-    return (
+// class Nav extends React.Component {
+//   render() {
+//     return (
 
-      <div>
+//       <div>
 
-        <div className="menu-button">
-          <i class="fas fa-bars"></i>
-        </div>
+//         <div className="menu-button">
+//           <i class="fas fa-bars"></i>
+//         </div>
 
-        {/* Sidebar */}
-        { menuData.length && (
-          <ul className="menu-items">
-            {menuData.map( item => (
-              <li key={item.label}>
-                <a href={item.url}>{item.label}</a>
-              </li>
-            ))}
-          </ul>
-        ) }
+//         {/* Sidebar */}
+//         { menuData.length && (
+//           <ul className="menu-items">
+//             {menuData.map( item => (
+//               <li key={item.label}>
+//                 <a href={item.url}>{item.label}</a>
+//               </li>
+//             ))}
+//           </ul>
+//         ) }
 
-      </div>
+//       </div>
 
-    )
-  }
-}
+//     )
+//   }
+// }
 
-export default Nav;
+// export default Nav;
