b48792d693bcfa3dc901e5cf8150632f69e50584
Julian <cusco2891@gmail.com>
2020-05-08 18:09:35 -0400

    components + about//working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Jumbotron from "./components/Jumbotron";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 // import Books from "./pages/Books";
-import Main from "./pages/Main";
+// import Main from "./pages/Main";
 
 
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
+import About from "./pages/About";
 
 
 
@@ -69,9 +70,9 @@ class App extends Component {
           {/* <Navbar /> */}
           <Wrapper>
             <Route exact path="/" component={About} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/discover" component={Discover} />
-            <Route exact path="/search" component={Search} />
+            {/* <Route exact path="/about" component={About} /> */}
+            {/* <Route exact path="/discover" component={Discover} /> */}
+            {/* <Route exact path="/search" component={Search} /> */}
           </Wrapper>
           <Footer />
         </div>
