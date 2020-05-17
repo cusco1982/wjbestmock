0f4cc40cf1289b5a0643ddb8fe56021bda6baafd
Julian <cusco2891@gmail.com>
2020-05-17 18:33:38 -0400

    sidedrawer links + portfolio page eworking

--- client/src/components/SideDrawer/SideDrawer.js
+++ client/src/components/SideDrawer/SideDrawer.js
@@ -17,6 +17,9 @@ const sideDrawer = props => {
           <a href="/about">About</a>
         </li>
         <li>
+          <a href="/portfolio">Portfolio</a>
+        </li>
+        <li>
           <a href="/contact">Contact</a>
         </li>
       </ul>
