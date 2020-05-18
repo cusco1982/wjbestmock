1def34e977e2c901747b37389fc995a58f2e7e7a
Julian <cusco2891@gmail.com>
2020-05-17 23:56:04 -0400

    need to fix button

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -4,31 +4,22 @@ import "./Slider.scss";
 
 
 
-
-
 function Slider() {
-
-
-    let sliderArr = [1, 2, 3, 4, 5]
-
-
+    let sliderArr = [1, 2, 3, 4, 5];
 
     return (
         <div className="slider">
-            {
-                sliderArr.map((item, index) => {
+            {sliderArr.map((item, index) => {
                     return (
                         <div key={index} className="slide">
                             {item}
                         </div>
-                    )
-                })
-            }
+                    );
+                })}
             <button id="goLeft">left</button>
             <button id="goRight">right</button>
-
         </div>
-    )
+    );
 }
 
 export default Slider;
