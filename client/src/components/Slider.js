5699aa70e3341825ae3f413f531a9b29ad873ac6
Julian <cusco2891@gmail.com>
2020-05-18 21:45:23 -0400

    slider working

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -9,11 +9,12 @@ function Slider() {
     let sliderArr = [1, 2, 3, 4, 5];
     const [x, setX] = useState(0);
     const goLeft = () => {
-        console.log(x);
-        setX(x + 100);
+        // console.log(x);
+        x === 0 ? setX( -100 * (sliderArr.length - 1)) : setX(x+100);
     };
     const goRight = () => {
-        setX(x - 100)
+        // console.log(x);
+        x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100)
     };
 
 
