fc423956a8a07bb1cf60ec3e227e5d4dacf1176c
Julian <cusco2891@gmail.com>
2020-05-18 21:38:54 -0400

    slider transform works

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./Slider.scss";
 
 
 
 
 function Slider() {
-    let sliderArr = [1, 2, 3, 4, 5];
-    const [x,setX] = useState(0)
 
+    let sliderArr = [1, 2, 3, 4, 5];
+    const [x, setX] = useState(0);
     const goLeft = () => {
-        setX(x+100)
+        console.log(x);
+        setX(x + 100);
     };
     const goRight = () => {
-        setX(x-100)
+        setX(x - 100)
     };
 
 
@@ -20,7 +21,10 @@ function Slider() {
         <div className="slider">
             {sliderArr.map((item, index) => {
                 return (
-                    <div key={index} className="slide" style={{transfrom:`translateX(${x}%)`}}>
+                    <div
+                        key={index}
+                        className="slide"
+                        style={{ transform: `translateX(${x}%)` }}>
                         {item}
                     </div>
                 );
