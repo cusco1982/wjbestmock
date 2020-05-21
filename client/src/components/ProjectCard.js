3b437cb1e09c287eaab0194b39d4f3bd23e3523b
Julian <cusco2891@gmail.com>
2020-05-21 18:57:36 -0400

    services

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Col from "./Col";
 
-function ProjectCard({src}) {
+function ProjectCard({ src }) {
     return (
-        <Col size="md-4" style={{float:"left", paddingBottom:"20px",paddingTop:"20px"}}>
+        <Col size="md-4" style={{ float: "left", paddingBottom: "20px", paddingTop: "20px" }}>
 
 
-            <div className="animated fadeInUp delay-1s" style={{cursor:"pointer"}}>
-                <img style={{height:"100%", width:"100%"}} src={src} className="" alt="WJ Best Insulation" />
+            <div className="animated fadeInUp delay-1s" style={{ cursor: "pointer" }}>
+                <header>Board Insulation</header>
+                <img style={{ height: "100%", width: "100%" }} src={src} className="" alt="WJ Best Insulation" />
             </div>
 
         </Col>
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 import Container from "../components/Container";
 import ImgGrid from "../components/ImgGrid";
+import Col from "../components/Col";
+import Row from "../components/Row";
+
+import Slider from "../components/Slider";
 
 
 
@@ -14,9 +18,37 @@ class Services extends Component {
 
     return (
       <div>
-        
-        <Container>
+
+
+
+        <Container style={{ paddingTop: "20px" }}>
+
+
+
+
+          <h1 style={{ textAlign: "center", paddingTop: "20px" }}>Services</h1>
+
           <ImgGrid />
+
+          <Row style={{paddingTop:"40px"}}>
+            <Col size="md-6" style={{justifyContent:"center",display:"flex", fontSize:"30px"}}>
+              <ul>
+                <li>Service1</li>
+                <li>Service2</li>
+                <li>Service3</li>
+              </ul>
+            </Col>
+            <Col size="md-6" style={{justifyContent:"center",display:"flex", fontSize:"30px"}}>
+              <ul>
+                <li>Service4</li>
+                <li>Service5</li>
+                <li>Service6</li>
+              </ul>
+            </Col>
+          </Row>
+
+          <Slider/>
+
         </Container>
 
       </div>
