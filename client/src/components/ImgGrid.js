7c4b90d04a31214116eab3dc06eb7e2ba238e60b
Julian <cusco2891@gmail.com>
2020-05-21 18:19:16 -0400

    img grid

---
+++ client/src/components/ImgGrid.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import i2 from "../pics/fantasy.jpg";
+import i3 from "../pics/trees.jpg";
+import i4 from "../pics/night.jpg";
+import i5 from "../pics/astronaut.jpg";
+
+import ProjectCard from "./ProjectCard";
+
+// import Container from "./Container";
+import Row from "./Row";
+
+
+
+class ImgGrid extends Component {
+
+  state = {
+    images: [
+      i5,
+      i2,
+      i3,
+      i4,
+      i5,
+      i2
+    ]
+  };
+
+
+  render() {
+
+    return (
+
+          <Row>
+            {this.state.images.map((item, index) => {
+              return <ProjectCard key={index} src={item} />
+
+            })}
+          </Row>
+    );
+  }
+}
+
+
+
+export default ImgGrid;
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,45 +1,27 @@
 import React, { Component } from "react";
 
-import i2 from "../pics/fantasy.jpg";
-import i3 from "../pics/trees.jpg";
-import i4 from "../pics/night.jpg";
-import i5 from "../pics/astronaut.jpg";
-
-import ProjectCard from "../components/ProjectCard";
-
 import Container from "../components/Container";
-import Row from "../components/Row";
+import ImgGrid from "../components/ImgGrid";
+
 
 
 
 class Services extends Component {
 
-  state = {
-    images: [
-      i5,
-      i2,
-      i3,
-      i4,
-      i5,
-      i2
-    ]
-  };
 
 
   render() {
 
     return (
+      <div>
+        
+        <Container>
+          <ImgGrid />
+        </Container>
 
-          <Row>
-            {this.state.images.map((item, index) => {
-              return <ProjectCard key={index} src={item} />
-
-            })}
-          </Row>
+      </div>
     );
   }
 }
-
-
 
 export default Services;
