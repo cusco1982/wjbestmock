e4325946ec421d55d32463d4e3431bd4b4aee99b
Julian <cusco2891@gmail.com>
2020-05-20 22:50:04 -0400

    check

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function ProjectCard(props) {
+function ProjectCard(src) {
     return (
         <div className="col-sm-4">
             <div className="animated fadeInUp delay-1s" style={{cursor:"pointer"}}>
-                <img style={{height:"700px", width:"500px", paddingBottom:"125px"}} src={props.image} className="card-img-top" alt="WJ Best Insulation" />
+                <img style={{height:"700px", width:"500px", paddingBottom:"125px"}} src={src} className="card-img-top" alt="WJ Best Insulation" />
             </div>
         </div>
     );
