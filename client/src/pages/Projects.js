30d438b01677e79f1a94c5a300dd27e417ce8dcf
Julian <cusco2891@gmail.com>
2020-05-22 03:21:19 -0400

    cont

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -23,8 +23,9 @@ class Projects extends Component {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, atque. Rerum minus laborum corrupti amet? Similique enim ullam ducimus repellat laboriosam maxime ab repudiandae! Inventore harum, qui quae exercitationem excepturi nostrum sapiente reiciendis accusantium autem dolore eos molestiae aperiam ab optio labore officia soluta obcaecati nulla dolor nesciunt est voluptatibus voluptates neque. Non, facilis saepe distinctio eum quidem consequuntur delectus tempora, necessitatibus minima voluptatem dolorem soluta! Nulla, aut vel cum iure eaque cumque alias in laborum ad explicabo voluptate delectus veniam laboriosam vero facere, culpa dolorum totam recusandae a velit magnam eius! Rem dolorem reprehenderit error! Tempora iusto veniam quibusdam?
           </Container>
 
-          <ImgGrid />
-          
+          <Container>
+            <ImgGrid />
+          </Container>
 
         </Wrapper>
       </div>
