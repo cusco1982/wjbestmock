60e00db443a556a08b7ad1dc169b358ec60d6028
Julian <cusco2891@gmail.com>
2020-05-21 21:59:53 -0400

    servicecard

--- client/src/components/ServiceCard/ServiceCard.js
+++ client/src/components/ServiceCard/ServiceCard.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Col from "../Col";
 import "./ServiceCard.css";
+import Container from "../Container";
+
 
 function ServiceCard({ src }) {
     return (
         <Col size="md-4" style={{ float: "left", paddingBottom: "20px", paddingTop: "20px" }}>
+            <Container>
 
-            <div className="animated fadeInUp delay-1s">
-                <header>Board Insulation</header>
-                <img style={{ height: "100%", width: "100%" }} src={src} className="" alt="WJ Best Insulation" />
-            </div>
+                    <header>Board Insulation</header>
+                    <img style={{ height: "100%", width: "100%" }} src={src} className="" alt="WJ Best Insulation" />
+                    <div className="middle">
+                        <div className="inside-text">
+                            Hello World
+                        </div>
+                    </div>
 
+            </Container>
         </Col>
     );
 }
