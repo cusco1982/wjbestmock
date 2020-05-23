9e625b39a1d851ee3cb2530640462973717836c6
Julian <cusco2891@gmail.com>
2020-05-23 00:38:04 -0400

    gallery

--- client/src/components/Gallery/Thumbnail.js
+++ client/src/components/Gallery/Thumbnail.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import ActiveWindow from './ActiveWindow';
-import ThumbnailGrid from './ThumbnailGrid';
 
 
-
-export default class ThumbnailGallery extends Component {
-    render() {
+const Thumbnail = () => {
         return (
-            <div style={{ flex: 1 }} >
-                <ActiveWindow />
-                <ThumbnailGrid />
-            </div>
 
-            <div style={{ flex: 1 }} >
-                Right
+            <div style={styles} >
+                Thumbnail
             </div>
         )
     }
 }
 
 const styles = {
-    background: "#ddd"
-    height: "500px",
-    width: "1024px",
-    margin: "40px auto",
-    display: "flex"
+    height: "35%",
+    width: "25%",
+    background: "red"
 }
 
-export default ActiveWindow;
+export default Thumbnail;
