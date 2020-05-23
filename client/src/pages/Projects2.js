d5d58d42e5da324a5ac3e1c77831df37ae6bcbe6
Julian <cusco2891@gmail.com>
2020-05-22 23:43:31 -0400

    thumbnail gallery

--- client/src/pages/Projects2.js
+++ client/src/pages/Projects2.js
@@ -16,13 +16,11 @@ class Projects2 extends Component {
       <div>
         <Wrapper>
 
-          <Container>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, atque. Rerum minus laborum corrupti amet? Similique enim ullam ducimus repellat laboriosam maxime ab repudiandae! Inventore harum, qui quae exercitationem excepturi nostrum sapiente reiciendis accusantium autem dolore eos molestiae aperiam ab optio labore officia soluta obcaecati nulla dolor nesciunt est voluptatibus voluptates neque. Non, facilis saepe distinctio eum quidem consequuntur delectus tempora, necessitatibus minima voluptatem dolorem soluta! Nulla, aut vel cum iure eaque cumque alias in laborum ad explicabo voluptate delectus veniam laboriosam vero facere, culpa dolorum totam recusandae a velit magnam eius! Rem dolorem reprehenderit error! Tempora iusto veniam quibusdam?
-          </Container>
+            <Row>
 
-          <Row>
-            <ImgGrid />
-          </Row>
+                     Hello0o0o00
+
+            </Row>
 
         </Wrapper>
       </div>
