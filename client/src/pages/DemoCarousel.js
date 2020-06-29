04bd93a400abdaea651dc0c12d2caf37277157e9
Julian <cusco2891@gmail.com>
2020-06-28 20:46:09 -0400

    focusing main page

--- client/src/pages/DemoCarousel.js
+++ client/src/pages/DemoCarousel.js
@@ -3,43 +3,47 @@ import React, { Component } from 'react';
 // import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 // import { Carousel } from 'react-responsive-carousel';
 // import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Container from "../components/Container"
+import Container from "../components/Container";
+import Row from "../components/Row";
+
 
 
 class DemoCarousel extends Component {
     render() {
         return (
             <div>
-                <Container>
+                <Row>
+                    <Container>
 
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="https://i.pinimg.com/originals/c6/04/b4/c604b48cf7f3e150cb5fe7215481bb13.jpg" alt="First slide" />
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators" style={{backgroundColor:"red"}}>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="https://i.pinimg.com/originals/c6/04/b4/c604b48cf7f3e150cb5fe7215481bb13.jpg" alt="First slide" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="https://images-na.ssl-images-amazon.com/images/I/61iqzUF0dzL._AC_SX522_.jpg" alt="Second slide" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="https://images-na.ssl-images-amazon.com/images/I/61iqzUF0dzL._AC_SX522_.jpg" alt="Third slide" />
+                                </div>
                             </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="https://i.pinimg.com/originals/c6/04/b4/c604b48cf7f3e150cb5fe7215481bb13.jpg" alt="Second slide" />
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="https://i.pinimg.com/originals/c6/04/b4/c604b48cf7f3e150cb5fe7215481bb13.jpg" alt="Third slide" />
-                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
 
-                </Container>
+                    </Container>
+                </Row>
             </div>
 
         );
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -11,34 +11,19 @@ function Main() {
         <h1>Pupster</h1>
         <h2>They're the Good Boys and Girls</h2>
       </Hero>
-      <Container style={{ marginTop: 30 }}>
+      <Container style={{ marginTop:30, backgroundColor:"red" }}>
         <Row>
           <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
+            <h1>Welcome To WJ Best Insulation!</h1>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+              At WJ Best Insulation we specialize in all things insulation!
             </p>
             <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+              Here's a view of some of our work. Click on the link to view more!
             </p>
             <p>
               Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
