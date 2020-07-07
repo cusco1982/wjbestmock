7c7614d4e5c4dbfb0dd0cc3ed85eebe001c986e1
Julian <cusco2891@gmail.com>
2020-07-07 19:53:17 -0400

    jumbo

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -13,7 +13,7 @@ function Main() {
         <h2>Home to New Jersey's Best Insulators</h2>
       </Hero> */}
 
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style={{height:"500px"}}>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style={{height:"700px"}}>
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
