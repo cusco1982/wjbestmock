b78c94b7c99ec8eddc08b8b6f2bd173d8fa03539
Julian <cusco2891@gmail.com>
2020-09-10 21:26:55 -0400

    jumbo

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -11,62 +11,71 @@ import "./Main.css";
 
 class Main extends Component {
   render() {
-    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let d;
     d = new Date();
-
     return (
-
       <div style={{ overflowX: "hidden" }}>
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style={{ height: "700px", backgroundColor: "orange", padding: "30px" }}>
+
+
+        {/* --------------------------------------------        --------------------------------------------        -------------------------------------------- */}
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           </ol>
+          {/* -------------------------------------------- */}
+
+
+          {/* -------------------------------------------- */}
           <div class="carousel-inner">
 
 
-
-            <div class="carousel-item active" >
-              <img class="d-block w-100" src="https://www.easterninsulation.com/wp-content/uploads/2016/04/BASF-1.jpg" alt="First slide" />
-
-              <Row style={{ padding: "20px" }}>
-                <a href="/projects" style={{ textAlign: "center", borderRadius: "25px", border: "2px solid white", padding: "20px", width: "200px", height: "75px" }}>
-                  VIEW PROJECT
-                </a>
-              </Row>
-
-              <Row>
-                &nbsp; &nbsp; &nbsp; {months[d.getMonth()]} {d.getFullYear()}
-              </Row>
-
-
-
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://www.easterninsulation.com/wp-content/uploads/2016/04/BASF-1.jpg" alt="Second slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h2>
+                  {months[d.getMonth()]} {d.getFullYear()}
+                </h2>
+                <p>
+                  <a href="/projects" style={{ textAlign: "center", borderRadius: "25px", border: "2px solid white", padding: "20px", width: "200px", height: "75px" }}>
+                    VIEW PROJECT
+                  </a>
+                </p>
+              </div>
             </div>
+
+
+            <div class="carousel-item" >
+              <img class="d-block w-100" src="https://www.easterninsulation.com/wp-content/uploads/2016/04/BASF-1.jpg" alt="Third slide" />
+            </div>
+
 
 
 
             <div class="carousel-item">
-              <img class="d-block w-100" src="" alt="Second slide" />
+              <img class="d-block w-100" src="https://www.easterninsulation.com/wp-content/uploads/2016/04/BASF-1.jpg" alt="Third slide" />
             </div>
-
-
-            <div class="carousel-item">
-              <img class="d-block w-100" src="https://www.easterninsulation.com/wp-content/uploads/2016/04/blueprintslider1.jpg" alt="Third slide" />
-            </div>
-
 
           </div>
-          {/* <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a> */}
+          {/* -------------------------------------------- */}
+
+
+          {/* -------------------------------------------- */}
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+
+
         </div>
 
         {/* --------------------------------------------        --------------------------------------------        -------------------------------------------- */}
